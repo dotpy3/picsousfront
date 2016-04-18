b0dccbf812eebe2f6a6660c3fb8c401b456a6d91
Eric Gourlaouen <e.gourlaouen@weezevent.com>
2016-04-18 11:01:21 +0200

    added perm autocomplete

--- app/controllers/addperm.js
+++ app/controllers/addperm.js
@@ -1,5 +1,40 @@
 angular.module('picsousApp').controller('AddPermCtrl', function($scope, APP_URL, message, $http, dateWrapper, $location, loadingSpin){
 	$scope.perm = {};
+	$scope.gettingPerm = false;
+	var dateAjd = new Date();
+
+	if (dateAjd.getHours() < 11) {
+		$scope.perm.periode = 'M';
+	} else if (dateAjd.getHours() < 15) {
+		$scope.perm.periode = 'D';
+	} else if (dateAjd.getHours() < 20) {
+		$scope.perm.periode = 'S';
+	}
+	$scope.perm.date = dateAjd;
+
+	$scope.findPerm = function(item) {
+		var permInSearch = item;
+		$scope.permToFind = item.text;
+		$scope.gettingPerm = true;
+		loadingSpin.start();
+		$http({
+			method: 'GET',
+			url: APP_URL + '/perms/' + item.id + '/',
+		}).then(function(response) {
+			var oldPerm = response.data;
+			$scope.perm.nom = oldPerm.nom;
+			$scope.perm.nom_resp = oldPerm.nom_resp;
+			$scope.perm.tel_resp = oldPerm.tel_resp;
+			$scope.perm.mail_resp = oldPerm.mail_resp;
+			$scope.perm.asso = oldPerm.asso;
+			$scope.perm.role = oldPerm.role;
+			loadingSpin.end();
+			$scope.gettingPerm = false;
+		}, function() {
+			$scope.gettingPerm = false;
+			loadingSpin.end();
+		});
+	};
 
 	$scope.addPerm = function() {
 		$scope.adding = true;
