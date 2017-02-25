609848009b164f783e35efff730b2b9e04208c5b
Eric Gourlaouen <gourlaoueneric@gmail.com>
2017-02-25 19:24:39 +0100

    changed http method to patch

--- app/controllers/backoffice.js
+++ app/controllers/backoffice.js
@@ -97,7 +97,7 @@ angular.module('picsousApp').controller('BackofficeCtrl', function($http, $scope
     
     $scope.giveRight = function(user, right) {
         user.changingRight = true;
-        $http({ method: 'PUT', url: APP_URL + '/userright/' + user.id + '/', data: {
+        $http({ method: 'PATCH', url: APP_URL + '/userright/' + user.id + '/', data: {
             id: user.id, right: right
         }}).then(function(response) {
             user.right = response.data.right;
